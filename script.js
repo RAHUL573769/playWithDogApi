d7dfbce3aa0a99f295ad34b7edc5a32a73c09539
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-24 16:53:39 +0600

    fhhhhhw commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function loadDogs() {
+function loadDogs(){
     fetch('https://api.thedogapi.com/v1/breeds')
 .then(response=>response.json())
 .then(data=>displayDogs(data))
@@ -11,12 +11,21 @@ function displayDogs(dogList) {
     // console.log(dogList);
 
     const main=document.getElementById('main');
-    const first10Data=dogList.slice(0,10);
+    const first10Data=dogList.slice(10,100);
 
-    const div=document.createElement('div');
-
+  
     for(const dog of first10Data){
-      
+        const div=document.createElement('div');
+        div.className="col-lg-3"
+    
+        // console.log(dog);
+      div.innerHTML=`
+      <h2>${dog.name}</h2>
+      <p>${dog.temperament}</p>
+      <h4>${dog.weight.imperial}</h4>
+      <img width="400px" height="250px" src="${dog.image.url}"/>
+      `
+      main.appendChild(div)
     }
    
 }
